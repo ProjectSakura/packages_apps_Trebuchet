crdroidandroid/android_packages_apps_Launcher3

Launcher3: Add restart action for toggling settings
Useful to update some settings that only change on restarting the launcher.

It's a clean up version of the following commits:
https://github.com/DirtyUnicorns/android_packages_apps_Launcher3/commit/10529fedf53e6f79abe380832dd5e7deca329b14
https://github.com/DirtyUnicorns/android_packages_apps_Launcher3/commit/a3357bb5f9defef8d91f4f2b663d45e422038447
https://github.com/DirtyUnicorns/android_packages_apps_Launcher3/commit/74cba5a81ae39c6694bc4b7ddc8cd64440c57bfa
https://github.com/DirtyUnicorns/android_packages_apps_Launcher3/commit/81e08c2033400625c0701a6bcb5cd9fd4504f5fe

Co-authored-by: Alex Cruz <alex@dirtyunicorns.com>
Co-authored-by: Ezio Lacandia Bijelkic <brabus84@gmail.com>

src/com/android/launcher3/Utilities.java
@@ -64,6 +64,7 @@ import android.view.View;
 import android.view.ViewConfiguration;
 import android.view.animation.Interpolator;
 
+import com.android.launcher3.LauncherModel;
 import com.android.launcher3.compat.LauncherAppsCompat;
 import com.android.launcher3.compat.ShortcutConfigActivityInfo;
 import com.android.launcher3.config.FeatureFlags;
@@ -74,6 +75,7 @@ import com.android.launcher3.icons.LauncherIcons;
 import com.android.launcher3.shortcuts.DeepShortcutManager;
 import com.android.launcher3.shortcuts.ShortcutKey;
 import com.android.launcher3.util.IntArray;
+import com.android.launcher3.util.LooperExecutor;
 import com.android.launcher3.util.PackageManagerHelper;
 import com.android.launcher3.views.Transposable;
 import com.android.launcher3.widget.PendingAddShortcutInfo;
@@ -121,6 +123,8 @@ public final class Utilities {
             Build.VERSION.SDK_INT >= Build.VERSION_CODES.O;
 
     public static final int SINGLE_FRAME_MS = 16;
+
+    private static final long WAIT_BEFORE_RESTART = 250;
 
     /**
      * Set on a motion event dispatched from the nav bar. See {@link MotionEvent#setEdgeFlags(int)}.
@@ -780,4 +784,15 @@ public final class Utilities {
             return mSize;
         }
     }
+
+    public static void restart(final Context context) {
+        new LooperExecutor(LauncherModel.getWorkerLooper()).execute(() -> {
+            try {
+                Thread.sleep(WAIT_BEFORE_RESTART);
+            } catch (Exception ignored) {
+            }
+            android.os.Process.killProcess(android.os.Process.myPid());
+        });
+    }
+
 }

ProjectSakura/packages_apps_Trebuchet

Change-Id: Iac6e61cc00a7afb57ff083f4b065c86d2a7f2c13
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
Signed-off-by: LordShenron <shen.priyanshu@gmail.com>

src/com/android/launcher3/Utilities.java
@@ -65,6 +65,7 @@ import android.view.animation.Interpolator;
 
 import androidx.core.os.BuildCompat;
 
+import com.android.launcher3.LauncherModel;
 import com.android.launcher3.config.FeatureFlags;
 import com.android.launcher3.dragndrop.FolderAdaptiveIcon;
 import com.android.launcher3.graphics.GridOptionsProvider;
@@ -78,6 +79,7 @@ import com.android.launcher3.pm.ShortcutConfigActivityInfo;
 import com.android.launcher3.shortcuts.ShortcutKey;
 import com.android.launcher3.shortcuts.ShortcutRequest;
 import com.android.launcher3.util.IntArray;
+import com.android.launcher3.util.LooperExecutor;
 import com.android.launcher3.util.PackageManagerHelper;
 import com.android.launcher3.widget.PendingAddShortcutInfo;
 
@@ -87,6 +89,8 @@ import java.util.List;
 import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import static com.android.launcher3.util.Executors.MODEL_EXECUTOR;
 
 /**
  * Various utilities shared amongst the Launcher's classes.
@@ -118,6 +122,8 @@ public final class Utilities {
 
     public static final boolean ATLEAST_OREO =
             Build.VERSION.SDK_INT >= Build.VERSION_CODES.O;
+
+    private static final long WAIT_BEFORE_RESTART = 250;
 
     /**
      * Set on a motion event dispatched from the nav bar. See {@link MotionEvent#setEdgeFlags(int)}.
@@ -694,5 +700,15 @@ public final class Utilities {
     public static boolean isWorkspaceEditAllowed(Context context) {
         SharedPreferences prefs = getPrefs(context.getApplicationContext());
         return prefs.getBoolean(InvariantDeviceProfile.KEY_WORKSPACE_EDIT, true);
+    }
+
+    public static void restart(final Context context) {
+        new LooperExecutor(MODEL_EXECUTOR.getLooper()).execute(() -> {
+            try {
+                Thread.sleep(WAIT_BEFORE_RESTART);
+            } catch (Exception ignored) {
+            }
+            android.os.Process.killProcess(android.os.Process.myPid());
+        });
     }
 }