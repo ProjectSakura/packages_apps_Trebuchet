017582537cde5488e5d46b9849a96846e5f101af
Suphon Thanakornpakapong <paphonb@gmail.com>
2021-10-18 12:06:07 +0700

    Launcher3: Add Google Lens scan button to recents
    And a squash of:
    
    Launcher3: Restart launcher on changing google search/lens app
    
    idoybh (YAAP): Keep clear all rightmost
    
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: hundeva <hundeva@gmail.com>
    Change-Id: If00a4542ff7134ce4b84de3894f800d2b9a4f797
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>

--- quickstep/src/com/android/quickstep/TaskOverlayFactory.java
+++ quickstep/src/com/android/quickstep/TaskOverlayFactory.java
@@ -345,6 +345,15 @@ public class TaskOverlayFactory implements ResourceBasedOverride {
             public void onClearAllTasksRequested() {
                 endLiveTileMode(TaskOverlay.this::clearAllTasks);
             }
+
+            @Override
+            public void onLens() {
+                if (mIsAllowedByPolicy) {
+                    endLiveTileMode(() -> mImageApi.startLensActivity());
+                } else {
+                    showBlockedByPolicyMessage();
+                }
+            }
         }
     }
 
@@ -360,5 +369,7 @@ public class TaskOverlayFactory implements ResourceBasedOverride {
         void onSplit();
 
         void onClearAllTasksRequested();
+
+        void onLens();
     }
 }
