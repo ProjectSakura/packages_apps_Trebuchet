30c930fd010fba4d458da586fe60f2db5ae5ae31
elpaablo <donopwhile1@gmail.com>
2022-10-27 03:32:27 +0100

    Launcher3: Switch to AppLock API for protected apps
    @neobuddy89: Add MANAGE_APP_LOCK permission to privapp_whitelist.
    
    Change-Id: I519dafb9b9e8df683df532dcaf35c29acb7306e5
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>

--- src/com/android/launcher3/lineage/LineageUtils.java
+++ src/com/android/launcher3/lineage/LineageUtils.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2019 The LineageOS Project
+ * Copyright (C) 2023 AlphaDroid
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.android.launcher3.lineage;
 
 import android.app.KeyguardManager;
@@ -73,4 +89,27 @@ public class LineageUtils {
         return keyguardManager != null && keyguardManager.isKeyguardSecure();
     }
 
+    public static boolean isSystemApp(Context context, String pkgName) {
+        try {
+            ApplicationInfo ai = context.getPackageManager().getApplicationInfo(pkgName, 0);
+            return ai.isSystemApp();
+        } catch (PackageManager.NameNotFoundException e) {
+            return false;
+        }
+    }
+
+    private static boolean isPackageWhitelisted(Context context, String pkgName) {
+        String[] whiteListedPackages = context.getResources().getStringArray(
+                com.android.internal.R.array.config_appLockAllowedSystemApps);
+        for (int i = 0; i < whiteListedPackages.length; i++) {
+            if (pkgName.equals(whiteListedPackages[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isPackageLockable(Context context, String pkgName) {
+        return !isSystemApp(context, pkgName) || isPackageWhitelisted(context, pkgName);
+    }
 }
