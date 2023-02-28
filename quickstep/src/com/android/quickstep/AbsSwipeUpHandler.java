78d9b1c4b0248786a88f27bcf9254688900283a4
Shen Lin <shhenlin@gmail.com>
2023-02-28 19:00:24 +0800

    Launcher3: Fix a potential NPE when buildAnimationController is called
    Fixes: 271010395
    Test: atest
    Change-Id: I581ba867bca6f3c8468d6e12f722cbfdd27e33a1
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>

--- quickstep/src/com/android/quickstep/AbsSwipeUpHandler.java
+++ quickstep/src/com/android/quickstep/AbsSwipeUpHandler.java
@@ -837,6 +837,9 @@ public abstract class AbsSwipeUpHandler<T extends StatefulActivity<S>,
         if (!canCreateNewOrUpdateExistingLauncherTransitionController()) {
             return;
         }
+        if (mActivity == null) {
+            return;
+        }
         initTransitionEndpoints(mActivity.getDeviceProfile());
         mAnimationFactory.createActivityInterface(mTransitionDragLength);
     }
