f57d5503df1a28e4280dce891ecf26a9fc759e0b
Dhina17 <dhinalogu@gmail.com>
2023-09-09 11:07:12 +0530

    Launcher3: Fix hotseat border space properly
    It fixes hotseat icons visibility in landscape.
    
    Change-Id: I7741b88cc3eda4d8e2c68eaee541e75ca685f906
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: ArmSM <armsmbuild1@gmail.com>
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>

--- src/com/android/launcher3/DeviceProfile.java
+++ src/com/android/launcher3/DeviceProfile.java
@@ -627,7 +627,7 @@ public class DeviceProfile {
                 || inv.inlineQsb[INDEX_TWO_PANEL_LANDSCAPE]
                 : inv.inlineQsb[INDEX_DEFAULT] || inv.inlineQsb[INDEX_LANDSCAPE])
                 && hotseatQsbHeight > 0;
-        isQsbInline = inv.inlineQsb[mTypeIndex] && canQsbInline;
+        isQsbInline = mIsScalableGrid && inv.inlineQsb[mTypeIndex] && canQsbInline;
 
         areNavButtonsInline = isTaskbarPresent && !isGestureMode;
         numShownHotseatIcons =
@@ -977,6 +977,8 @@ public class DeviceProfile {
      * necessary.
      */
     public void recalculateHotseatWidthAndBorderSpace() {
+        if (!mIsScalableGrid) return;
+
         updateHotseatWidthAndBorderSpace(inv.numColumns);
         int numWorkspaceColumns = getPanelCount() * inv.numColumns;
         if (isTwoPanels) {
