2c625c851b58879ed947ba5b5d67917b71420957
Pranav Vashi <neobuddy89@gmail.com>
2024-06-29 01:26:12 +0530

    Launcher3: Add toggle to use two lines for app label
    Change-Id: I40c11b04995649611c215de3cf1955dd86a45f3f
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: ArmSM <armsmbuild1@gmail.com>
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>

--- src/com/android/launcher3/InvariantDeviceProfile.java
+++ src/com/android/launcher3/InvariantDeviceProfile.java
@@ -349,6 +349,10 @@ public class InvariantDeviceProfile implements SafeCloseable, OnSharedPreference
 
     @Override
     public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
+        if (key.equals(LauncherPrefs.ENABLE_TWOLINE_ALLAPPS_TOGGLE.getKey())) {
+            onConfigChanged(mContext);
+            return;
+        }
 
         switch (key) {
             case KEY_ALLAPPS_THEMED_ICONS:
