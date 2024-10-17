3c9b24a5095ee76b29c5a468ca6e0bd4774e9524
Ido Ben-Hur <idoybh2@gmail.com>
2024-10-17 16:54:56 +0300

    Launcher3: Add an option to open the keyboard with the drawer
    Signed-off-by: ArmSM <armsmbuild1@gmail.com>
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -426,6 +426,8 @@ public class Launcher extends StatefulActivity<LauncherState>
     // QuickSpace
     private QuickSpaceView mQuickSpace;
 
+    private boolean mWasImeOpened = false;
+
     public static Launcher getLauncher(Context context) {
         return fromContext(context);
     }
@@ -2919,9 +2921,18 @@ public class Launcher extends StatefulActivity<LauncherState>
      * @param progress Transition progress from 0 to 1; where 0 => home and 1 => all apps.
      */
     public void onAllAppsTransition(float progress) {
+        if (progress == 1f && !mWasImeOpened &&
+                Utilities.enableAutoIme(mWorkspace.getContext())) {
+            ExtendedEditText editText = mAppsView.getSearchUiManager().getEditText();
+            if (editText == null) return;
+            editText.showKeyboard();
+            mWasImeOpened = true;
+            return;
+        }
         if (progress == 0 && mAppsView != null) {
             hideKeyboard();
         }
+        mWasImeOpened = false;
     }
 
     /**
--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -204,6 +204,7 @@ public final class Utilities {
     public static final String KEY_SHOW_QUICKSPACE_WEATHER_TEXT = "pref_quickspace_weather_text";
     public static final String KEY_DRAWER_SCROLLBAR = "pref_drawer_scrollbar";
     public static final String KEY_FORCE_MONOCHROME_ICON = "pref_force_monochrome_icon";
+    public static final String KEY_AUTO_KEYABORD = "pref_auto_keyboard";
 
     /**
      * Returns true if theme is dark.
@@ -1208,5 +1209,10 @@ public final class Utilities {
     public static boolean isForcedMonoIconEnabled(Context context) {
         SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
         return prefs.getBoolean(KEY_FORCE_MONOCHROME_ICON, false);
-   }
+    }
+
+    public static boolean enableAutoIme(Context context) {
+        SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
+        return prefs.getBoolean(KEY_AUTO_KEYABORD, false);
+    }
 }
