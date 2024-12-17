7ce1be29b29f3ac6b60f86361f9a5026bbe9fa8b
John Galt <johngaltfirstrun@gmail.com>
2024-12-17 15:18:13 -0500

    Revert "Launcher3: Remove blur animation on app launch/exit"
    This reverts commit 80bd11e323ac6c6504e9f03b1661219f262f5ed9.
    
    Does not work on qpr1, has unintended consequences
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: ArmSM <armsmbuild1@gmail.com>
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>

--- quickstep/src/com/android/launcher3/QuickstepTransitionManager.java
+++ quickstep/src/com/android/launcher3/QuickstepTransitionManager.java
@@ -898,7 +898,11 @@ public class QuickstepTransitionManager implements OnDeviceProfileChangeListener
 
         // If app targets are translucent, do not animate the background as it causes a visible
         // flicker when it resets itself at the end of its animation.
-        animatorSet.play(appAnimator);
+        if (appTargetsAreTranslucent || !launcherClosing) {
+            animatorSet.play(appAnimator);
+        } else {
+            animatorSet.playTogether(appAnimator, getBackgroundAnimator());
+        }
         return animatorSet;
     }
 
@@ -1034,7 +1038,11 @@ public class QuickstepTransitionManager implements OnDeviceProfileChangeListener
 
         // If app targets are translucent, do not animate the background as it causes a visible
         // flicker when it resets itself at the end of its animation.
-        animatorSet.play(appAnimator);
+        if (appTargetsAreTranslucent || !launcherClosing) {
+            animatorSet.play(appAnimator);
+        } else {
+            animatorSet.playTogether(appAnimator, getBackgroundAnimator());
+        }
         return animatorSet;
     }
 
@@ -1618,6 +1626,12 @@ public class QuickstepTransitionManager implements OnDeviceProfileChangeListener
                 } else {
                     anim.play(new StaggeredWorkspaceAnim(mLauncher, velocity.y,
                             true /* animateOverviewScrim */, launcherView).getAnimators());
+                }
+
+                if (!areAllTargetsTranslucent(appTargets)) {
+                    anim.play(ObjectAnimator.ofFloat(mLauncher.getDepthController().stateDepth,
+                            MULTI_PROPERTY_VALUE,
+                            BACKGROUND_APP.getDepth(mLauncher), NORMAL.getDepth(mLauncher)));
                 }
 
                 // We play StaggeredWorkspaceAnim as a part of the closing window animation.
