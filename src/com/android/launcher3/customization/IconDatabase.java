crdroidandroid/android_packages_apps_Launcher3

Launcher3: Switch to icon pack theming preview
Ref:
https://github.com/crdroidandroid/android_packages_apps_Launcher3/commit/8155a4f3db5888b578aecb8904529a22f962968e
https://github.com/crdroidandroid/android_packages_apps_Launcher3/commit/174d11c6fa88e85920ec078af506fc56ef3bccd4

Tweaked to our usage

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

src/com/android/launcher3/customization/IconDatabase.java
@@ -2,25 +2,46 @@ package com.android.launcher3.customization;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 
 import com.android.launcher3.BuildConfig;
+import com.android.launcher3.R;
+import com.android.launcher3.Utilities;
 import com.android.launcher3.util.ComponentKey;
 
 public class IconDatabase {
+
     private static final String PREF_FILE_NAME = BuildConfig.APPLICATION_ID + ".ICON_DATABASE";
-    private static final String KEY_GLOBAL = "global";
-    private static final String VALUE_DEFAULT = "";
+    public static final String KEY_ICON_PACK = "pref_icon_pack";
+    public static final String VALUE_DEFAULT = "";
 
     public static String getGlobal(Context context) {
-        return getIconPackPrefs(context).getString(KEY_GLOBAL, VALUE_DEFAULT);
+        return Utilities.getPrefs(context).getString(KEY_ICON_PACK, VALUE_DEFAULT);
+    }
+
+    public static String getGlobalLabel(Context context) {
+        final String defaultLabel = context.getString(R.string.icon_pack_default_label);
+        final String pkgName = getGlobal(context);
+        if (VALUE_DEFAULT.equals(pkgName)) {
+            return defaultLabel;
+        }
+
+        final PackageManager pm = context.getPackageManager();
+        try {
+            final ApplicationInfo ai = pm.getApplicationInfo(pkgName, 0);
+            return (String) pm.getApplicationLabel(ai);
+        } catch (PackageManager.NameNotFoundException e) {
+            return defaultLabel;
+        }
     }
 
     public static void setGlobal(Context context, String value) {
-        getIconPackPrefs(context).edit().putString(KEY_GLOBAL, value).apply();
+        Utilities.getPrefs(context).edit().putString(KEY_ICON_PACK, value).apply();
     }
 
     public static void resetGlobal(Context context) {
-        getIconPackPrefs(context).edit().remove(KEY_GLOBAL).apply();
+        Utilities.getPrefs(context).edit().remove(KEY_ICON_PACK).apply();
     }
 
     public static String getByComponent(Context context, ComponentKey key) {
@@ -33,10 +54,6 @@ public class IconDatabase {
 
     public static void resetForComponent(Context context, ComponentKey key) {
         getIconPackPrefs(context).edit().remove(key.toString()).apply();
-    }
-
-    public static void clearAll(Context context) {
-        getIconPackPrefs(context).edit().clear().apply();
     }
 
     private static SharedPreferences getIconPackPrefs(Context context) {

src/com/android/launcher3/icons/pack/IconPackHeaderPreference.java
@@ -1,0 +1,101 @@
+/*
+ * Copyright (C) 2020 Shift GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.launcher3.icons.pack;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.LauncherApps;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.view.View;
+import android.widget.ImageView;
+import android.util.AttributeSet;
+import android.util.Log;
+
+import androidx.core.content.res.TypedArrayUtils;
+import androidx.preference.CheckBoxPreference;
+import androidx.preference.PreferenceViewHolder;
+
+import com.android.launcher3.R;
+import com.android.launcher3.icons.ThirdPartyIconProvider;
+
+
+public class IconPackHeaderPreference extends RadioHeaderPreference {
+    private static final String TAG = "IconPackHeaderPreference";
+    private static final int PREVIEW_ICON_NUM = 8;
+    // This value has been selected as an average of usual "device profile-computed" values
+    private static final int PREVIEW_ICON_DPI = 500;
+
+    private final Context context;
+    private ImageView[] icons = null;
+
+    public IconPackHeaderPreference(Context context) {
+        this(context, null);
+    }
+
+    public IconPackHeaderPreference(Context context, AttributeSet attrs) {
+        this(context, attrs, TypedArrayUtils.getAttr(context,
+                androidx.preference.R.attr.preferenceStyle,
+                android.R.attr.preferenceStyle));
+    }
+
+    public IconPackHeaderPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        this.context = context;
+
+        setLayoutResource(R.layout.preference_widget_icons_preview);
+    }
+
+    @Override
+    public void onBindViewHolder(PreferenceViewHolder holder) {
+        final ImageView[] imageViews = {
+            (ImageView) holder.findViewById(R.id.pref_icon_a),
+            (ImageView) holder.findViewById(R.id.pref_icon_b),
+            (ImageView) holder.findViewById(R.id.pref_icon_c),
+            (ImageView) holder.findViewById(R.id.pref_icon_d),
+            (ImageView) holder.findViewById(R.id.pref_icon_e),
+            (ImageView) holder.findViewById(R.id.pref_icon_f),
+            (ImageView) holder.findViewById(R.id.pref_icon_g),
+            (ImageView) holder.findViewById(R.id.pref_icon_h)
+        };
+        this.icons = imageViews;
+        onRadioElementSelected(null);
+    }
+
+    @Override
+    public void onDetached() {
+        this.icons = null;
+        super.onDetached();
+    }
+
+    @Override
+    public void onRadioElementSelected(String key) {
+        if (icons == null || icons.length == 0) {
+            return;
+        }
+
+        final ThirdPartyIconProvider iconProvider = new ThirdPartyIconProvider(context);
+        final PackageManager pm = context.getPackageManager();
+        final LauncherApps launcherApps = context.getSystemService(LauncherApps.class);
+        new GetLaunchableInfoTask(pm, launcherApps, PREVIEW_ICON_NUM, (aiList) -> {
+            for (int i = 0; i < icons.length; i++) {
+                icons[i].setImageDrawable(iconProvider.getIcon(
+                        aiList.get(i), PREVIEW_ICON_DPI));
+            }
+        }).execute();
+    }
+}

ProjectSakura/packages_apps_Trebuchet

Change-Id: I82b16d0d4bf5f8e9ef1dca3dc64e4aa1714e04f1
Signed-off-by: ArmSM <armsmbuild1@gmail.com>
Signed-off-by: LordShenron <shen.priyanshu@gmail.com>

src/com/android/launcher3/customization/IconDatabase.java
@@ -2,25 +2,46 @@ package com.android.launcher3.customization;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 
 import com.android.launcher3.BuildConfig;
+import com.android.launcher3.LauncherPrefs;
+import com.android.launcher3.R;
 import com.android.launcher3.util.ComponentKey;
 
 public class IconDatabase {
+
     private static final String PREF_FILE_NAME = BuildConfig.APPLICATION_ID + ".ICON_DATABASE";
-    private static final String KEY_GLOBAL = "global";
-    private static final String VALUE_DEFAULT = "";
+    public static final String KEY_ICON_PACK = "pref_icon_pack";
+    public static final String VALUE_DEFAULT = "";
 
     public static String getGlobal(Context context) {
-        return getIconPackPrefs(context).getString(KEY_GLOBAL, VALUE_DEFAULT);
+        return LauncherPrefs.getPrefs(context).getString(KEY_ICON_PACK, VALUE_DEFAULT);
+    }
+
+    public static String getGlobalLabel(Context context) {
+        final String defaultLabel = context.getString(R.string.icon_pack_default_label);
+        final String pkgName = getGlobal(context);
+        if (VALUE_DEFAULT.equals(pkgName)) {
+            return defaultLabel;
+        }
+
+        final PackageManager pm = context.getPackageManager();
+        try {
+            final ApplicationInfo ai = pm.getApplicationInfo(pkgName, 0);
+            return (String) pm.getApplicationLabel(ai);
+        } catch (PackageManager.NameNotFoundException e) {
+            return defaultLabel;
+        }
     }
 
     public static void setGlobal(Context context, String value) {
-        getIconPackPrefs(context).edit().putString(KEY_GLOBAL, value).apply();
+        LauncherPrefs.getPrefs(context).edit().putString(KEY_ICON_PACK, value).apply();
     }
 
     public static void resetGlobal(Context context) {
-        getIconPackPrefs(context).edit().remove(KEY_GLOBAL).apply();
+        LauncherPrefs.getPrefs(context).edit().remove(KEY_ICON_PACK).apply();
     }
 
     public static String getByComponent(Context context, ComponentKey key) {
@@ -33,10 +54,6 @@ public class IconDatabase {
 
     public static void resetForComponent(Context context, ComponentKey key) {
         getIconPackPrefs(context).edit().remove(key.toString()).apply();
-    }
-
-    public static void clearAll(Context context) {
-        getIconPackPrefs(context).edit().clear().apply();
     }
 
     private static SharedPreferences getIconPackPrefs(Context context) {

src/com/android/launcher3/icons/pack/IconPackHeaderPreference.java
@@ -1,0 +1,101 @@
+/*
+ * Copyright (C) 2020 Shift GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.launcher3.icons.pack;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.LauncherApps;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.view.View;
+import android.widget.ImageView;
+import android.util.AttributeSet;
+import android.util.Log;
+
+import androidx.core.content.res.TypedArrayUtils;
+import androidx.preference.CheckBoxPreference;
+import androidx.preference.PreferenceViewHolder;
+
+import com.android.launcher3.R;
+import com.android.launcher3.icons.IconProvider;
+
+
+public class IconPackHeaderPreference extends RadioHeaderPreference {
+    private static final String TAG = "IconPackHeaderPreference";
+    private static final int PREVIEW_ICON_NUM = 8;
+    // This value has been selected as an average of usual "device profile-computed" values
+    private static final int PREVIEW_ICON_DPI = 500;
+
+    private final Context context;
+    private ImageView[] icons = null;
+
+    public IconPackHeaderPreference(Context context) {
+        this(context, null);
+    }
+
+    public IconPackHeaderPreference(Context context, AttributeSet attrs) {
+        this(context, attrs, TypedArrayUtils.getAttr(context,
+                androidx.preference.R.attr.preferenceStyle,
+                android.R.attr.preferenceStyle));
+    }
+
+    public IconPackHeaderPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        this.context = context;
+
+        setLayoutResource(R.layout.preference_widget_icons_preview);
+    }
+
+    @Override
+    public void onBindViewHolder(PreferenceViewHolder holder) {
+        final ImageView[] imageViews = {
+            (ImageView) holder.findViewById(R.id.pref_icon_a),
+            (ImageView) holder.findViewById(R.id.pref_icon_b),
+            (ImageView) holder.findViewById(R.id.pref_icon_c),
+            (ImageView) holder.findViewById(R.id.pref_icon_d),
+            (ImageView) holder.findViewById(R.id.pref_icon_e),
+            (ImageView) holder.findViewById(R.id.pref_icon_f),
+            (ImageView) holder.findViewById(R.id.pref_icon_g),
+            (ImageView) holder.findViewById(R.id.pref_icon_h)
+        };
+        this.icons = imageViews;
+        onRadioElementSelected(null);
+    }
+
+    @Override
+    public void onDetached() {
+        this.icons = null;
+        super.onDetached();
+    }
+
+    @Override
+    public void onRadioElementSelected(String key) {
+        if (icons == null || icons.length == 0) {
+            return;
+        }
+
+        final IconProvider iconProvider = IconProvider.INSTANCE.get(context);
+        final PackageManager pm = context.getPackageManager();
+        final LauncherApps launcherApps = context.getSystemService(LauncherApps.class);
+        new GetLaunchableInfoTask(pm, launcherApps, PREVIEW_ICON_NUM, (aiList) -> {
+            for (int i = 0; i < icons.length; i++) {
+                icons[i].setImageDrawable(iconProvider.getIcon(
+                        aiList.get(i), PREVIEW_ICON_DPI));
+            }
+        }).execute();
+    }
+}