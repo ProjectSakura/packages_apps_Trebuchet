fdbad6a2d28d5aae92a0141a4cac1b9824d65036
LuK1337 <priv.luk@gmail.com>
2025-03-25 20:13:40 +0100

    Launcher3: Add FORCE_SHOW_NAVBAR support
    Change-Id: I96d32d36d4182d9ecb3bc0d7036caaf540bd7b58
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- quickstep/src/com/android/launcher3/taskbar/TaskbarManager.java
+++ quickstep/src/com/android/launcher3/taskbar/TaskbarManager.java
@@ -130,6 +130,9 @@ public class TaskbarManager {
     public static final Uri GESTURE_NAVBAR_HEIGHT_MODE = Settings.System.getUriFor(
             Settings.System.GESTURE_NAVBAR_HEIGHT_MODE);
 
+    public static final Uri FORCE_SHOW_NAVBAR = LineageSettings.System.getUriFor(
+            LineageSettings.System.FORCE_SHOW_NAVBAR);
+
     private final Context mWindowContext;
     private final @Nullable Context mNavigationBarPanelContext;
     private WindowManager mWindowManager;
@@ -277,6 +280,8 @@ public class TaskbarManager {
                 .register(GESTURE_NAVBAR_LENGTH_MODE, mOnTaskBarChangeListener);
         SettingsCache.INSTANCE.get(mWindowContext)
                 .register(GESTURE_NAVBAR_HEIGHT_MODE, mOnTaskBarChangeListener);
+        SettingsCache.INSTANCE.get(mWindowContext)
+                .register(FORCE_SHOW_NAVBAR, mOnTaskBarChangeListener);
         Log.d(TASKBAR_NOT_DESTROYED_TAG, "registering component callbacks from constructor.");
         mWindowContext.registerComponentCallbacks(mDefaultComponentCallbacks);
         mShutdownReceiver.register(mWindowContext, Intent.ACTION_SHUTDOWN);
@@ -759,6 +764,8 @@ public class TaskbarManager {
                 .unregister(GESTURE_NAVBAR_LENGTH_MODE, mOnTaskBarChangeListener);
         SettingsCache.INSTANCE.get(mWindowContext)
                 .unregister(GESTURE_NAVBAR_HEIGHT_MODE, mOnTaskBarChangeListener);
+        SettingsCache.INSTANCE.get(mWindowContext)
+                .unregister(FORCE_SHOW_NAVBAR, mOnTaskBarChangeListener);
         Log.d(TASKBAR_NOT_DESTROYED_TAG, "unregistering component callbacks from destroy().");
         mWindowContext.unregisterComponentCallbacks(mDefaultComponentCallbacks);
         mShutdownReceiver.unregisterReceiverSafely(mWindowContext);
@@ -780,7 +787,8 @@ public class TaskbarManager {
 
     boolean hasNavigationBar() {
         return mActivity.getResources().getBoolean(
-                com.android.internal.R.bool.config_showNavigationBar);
+                com.android.internal.R.bool.config_showNavigationBar) ||
+                SettingsCache.INSTANCE.get(mWindowContext).getValue(FORCE_SHOW_NAVBAR, 0);
     }
 
     private void addTaskbarRootViewToWindow(int displayId) {
