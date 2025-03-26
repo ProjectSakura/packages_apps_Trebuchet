crdroidandroid/android_packages_apps_Launcher3

Launcher3: Add toggle disable hotseat searchbar
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

src/com/android/launcher3/Hotseat.java
@@ -62,7 +62,11 @@ public class Hotseat extends CellLayout implements Insettable {
     public Hotseat(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
 
-        mQsb = LayoutInflater.from(context).inflate(R.layout.search_container_hotseat, this, false);
+        if (Utilities.isQSBEnabled(context)) {
+            mQsb = LayoutInflater.from(context).inflate(R.layout.search_container_hotseat, this, false);
+        } else {
+            mQsb = LayoutInflater.from(context).inflate(R.layout.search_container_hotseat_noqsb, this, false);
+        }
         mQsbHeight = mQsb.getLayoutParams().height;
         addView(mQsb);
 

ProjectSakura/packages_apps_Trebuchet

Change-Id: I606ead4bad62ab8dff836f3fd0a4c2a6c4d786f9

src/com/android/launcher3/Hotseat.java
@@ -99,7 +99,11 @@ public class Hotseat extends CellLayout implements Insettable {
 
     public Hotseat(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-        mQsb = LayoutInflater.from(context).inflate(R.layout.search_container_hotseat, this, false);
+        if (Utilities.showQSB(context)) {
+            mQsb = LayoutInflater.from(context).inflate(R.layout.search_container_hotseat, this, false);
+        } else {
+            mQsb = LayoutInflater.from(context).inflate(R.layout.empty_view, this, false);
+        }
         addView(mQsb);
         mIconsAlphaChannels = new MultiValueAlpha(getShortcutsAndWidgets(),
                 ALPHA_CHANNEL_CHANNELS_COUNT);